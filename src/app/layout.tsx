50a3dc50374cd84170b301a098d2111dd2d991a6
Yoedi Arianto <yoediarianto@gmail.com>
2024-08-13 09:55:42 +0700

    slicing sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/layouts/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
           <div className="border-t">
             <div className="bg-background">
               <div className="flex flex-row">
-                <div className="hidden lg:block w-[18%]">SideBar</div>
+                <div className="hidden lg:block w-[18%]">
+                  <Sidebar />
+                </div>
                 <div className="col-span-3 overflow-auto lg:col-span-3 lg:border-l w-[82%]">
                   <div className="px-6 py-6 lg:px-8">
                     <div>Header</div>
---
+++ src/components/layouts/Sidebar/index.tsx
@@ -0,0 +1,90 @@
+import { Button } from "@/components/ui/button";
+import React, { FC } from "react";
+import {
+  AiOutlineHome,
+  AiOutlineMessage,
+  AiOutlineUser,
+  AiOutlineCalendar,
+  AiOutlineLogout,
+} from "react-icons/ai";
+import { BsBuildings, BsGear } from "react-icons/bs";
+import { HiOutlineClipboardList } from "react-icons/hi";
+
+interface SidebarProps {}
+
+const Sidebar: FC<SidebarProps> = ({}) => {
+  return (
+    <div className="pb-12 min-h-screen">
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold">Dashboard</h2>
+          <div className="space-y-3">
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start rounded-none hover:text-primary"
+            >
+              <AiOutlineHome className="mr-2 text-lg" />
+              Home
+            </Button>
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start rounded-none hover:text-primary"
+            >
+              <AiOutlineMessage className="mr-2 text-lg" />
+              Messages
+            </Button>
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start rounded-none hover:text-primary"
+            >
+              <BsBuildings className="mr-2 text-lg" />
+              Company Profile
+            </Button>
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start rounded-none hover:text-primary"
+            >
+              <AiOutlineUser className="mr-2 text-lg" />
+              All Aplicants
+            </Button>
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start rounded-none hover:text-primary"
+            >
+              <HiOutlineClipboardList className="mr-2 text-lg" />
+              Job Listing
+            </Button>
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start rounded-none hover:text-primary"
+            >
+              <AiOutlineCalendar className="mr-2 text-lg" />
+              My Schedule
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold">Settings</h2>
+          <Button
+            variant={"ghost"}
+            className="w-full justify-start rounded-none hover:text-primary"
+          >
+            <BsGear className="mr-2 text-lg" />
+            Setting
+          </Button>
+          <Button
+            variant={"ghost"}
+            className="w-full justify-start rounded-none text-red-500 hover:bg-red-200 hover:text-red-500"
+          >
+            <AiOutlineLogout className="mr-2 text-lg" />
+            Logout
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
